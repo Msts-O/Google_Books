5422849d60daed5a9d208c48600ace20777d6c76
Masatoshi <msts.oo0131@gmail.com>
2020-12-30 20:49:52 +0900

    route.tsx_compiled

---
+++ book_api/src/temporary.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export const temporary: React.FC= () => {
+    return<Wrapper>temporay</Wrapper>
+ };
+
+const Wrapper= styled.div``;
