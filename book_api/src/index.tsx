4fae7d21b8ae4018b4dfc08b97ec03f536f0ade8
Masatoshi <msts.oo0131@gmail.com>
2021-01-02 18:58:34 +0900

    yarn_start_enviroment_created

--- book_api/src/index.tsx
+++ book_api/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import routes from 'routes';
+const history = createBrowserHistory();
+
+ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('root'));
+
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ book_api/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import App from 'App';
+import { temporary } from './temporary';
+
+export const Path = {
+    app: '/',
+    temporary: '/temporary',
+};
+
+const routes = (
+    <Switch>
+        <Route exact path={Path.app} component={App} />
+        <Route exact path={Path.temporary} component={temporary} />
+        <Redirect to={Path.app} />
+    </Switch>
+);
+
+export default routes;
