bb308156278240cb12b368518af9649fb81c3064
Masatoshi <msts.oo0131@gmail.com>
2021-01-05 18:19:48 +0900

    google_book_api_successed

--- book_api/src/Temporary.tsx
+++ book_api/src/Temporary.tsx
@@ -1,16 +1,49 @@
 import React , { useState }from 'react';
 import styled from 'styled-components';
+import axios from "axios";
+
+const searchGoogleBooks = async (searchString: string) => {
+    const url = 'https://www.googleapis.com/books/v1/volumes';
+    const params = { q: searchString };
+    try {
+        const response = await axios.get(url, { params });
+        return { isSuccess: true, data: response.data, error: null };
+    }
+    catch (error) {
+          return { isSuccess: false, data: null, error };
+    }
+};
 
 export const Temporary: React.FC = () => {
-    const [text, changeText] = useState('');
+    const [searchString, changeSearchString] = useState('');
+    const [searchResult, changeSearchResult] = useState<any>(null);
+
+    const handleOnSearchButton = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault();
+        const result = await searchGoogleBooks(searchString);
+        if (result.isSuccess) {
+            changeSearchResult(result.data);
+        } else {
+            window.alert(String(result.error));
+        }
+    };
     return (
         <Wrapper>
             <Body>
-                <Title>Temporary Component</Title>
-
-                <TextArea placeholder='write some text！' onChange={(event): void => changeText(event.target.value)} />
-
-                <TextResult>{text}</TextResult>
+                <Title>Find books</Title>
+                <SearchForm>
+                 <Input placeholder='Type some words! ' onChange={event => changeSearchString(event.target.value)} />
+                    <SearchButton onClick={event => handleOnSearchButton(event)} disabled={!searchString}>
+                      Searching
+                    </SearchButton>
+                </SearchForm>
+                {searchResult && (
+                    <ResultContent>
+                        {searchResult.items.map((item: any) => {
+                            return <ResultTitle key={item.id}>{item.volumeInfo.title}</ResultTitle>;
+                        })}
+                    </ResultContent>
+                )}
             </Body>
         </Wrapper>
     );
@@ -27,18 +60,45 @@ const Title = styled.h1`
   text-align: center;
 `;
 
-const TextArea = styled.textarea`
+const Input = styled.input`
   display: block;
-  margin: 0 auto;
   box-sizing: border-box;
-  width: 200px;
+  width: 250px;
+  font-size: 18px;
+  padding: 10px;
+  outline: none;
+`;
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
 `;
 
-const TextResult = styled.p`
-  width: 200px;
+const SearchButton = styled.button`
+  color: #fff;
+  background-color: #09d3ac;
+  border-radius: 3px;
+  margin-left: 10px;
   padding: 10px;
-  margin: 20px auto;
-  border: 1px solid blue;
-  white-space: pre-wrap;
-  box-sizing: border-box;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+  cursor: pointer;
+  &:disabled {
+    background-color: #bfbfbf;
+    cursor: not-allowed;
+  }
+`;
+const ResultContent = styled.div`
+  margin-top: 20px;
+`;
+
+const ResultTitle = styled.div`
+  padding: 10px 0;
+  border-bottom: 1px solid;
+  &:first-of-type {
+    border-top: 1px solid;
+  }
 `;
